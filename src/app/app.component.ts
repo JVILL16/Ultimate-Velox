c11461c93a01a472e1e9c24eabd92c234cc6648c
Jheremi Villarreal <jheremi2015@gmail.com>
2019-08-28 11:40:25 -0500

    changing rules on tslint to make angular typescript run normally

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,15 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   
   title = 'utsa-velox';
+  
+  constructor(private router:Router){}
+
+  onClickProfile() {
+    this.router.navigate(['login-page']);
+  }
 
   ngOnInit(): void {
+    
     throw new Error("Method not implemented.");
   }
  
